7d2307af088669f326d14549d0985ebc2329f479
根荣 许 <1589575776@qq.com>
2023-03-09 17:46:14 +0800

    基本配置

--- hello_world.ts
+++ hello_world.ts
@@ -1,4 +1,10 @@
+// 编译命令：tsc hello_world.ts
+
 //输出hello world
 console.log("hello world一二三");
 //输出时间
 console.log(new Date());
+
+//节点形参（O）：--require ts-node/register
+//JavaScript文件（F）：$FilePathRelativeToProjectRoot$
+//应用程序形参（C）：--project tsconfig.json
